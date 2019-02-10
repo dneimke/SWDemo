1a356bdd884cab036452372f8b345063650a2ec5
Darren Neimke <darren.neimke@gmail.com>
2019-02-11 05:24:01 +1030

    Adds typed dialog with ko bindings

--- Scripts/PlannerTool.ts
+++ Scripts/PlannerTool.ts
@@ -5,38 +5,38 @@ export class PlannerTool {
     private _plannerContainer: HTMLElement;
     private _createButton: HTMLButtonElement;
     private _saveButton: HTMLButtonElement;
-    private _saveDialog: HTMLElement;
     private _state: ToolState;
+
+    private _dialog: CompletePlanDialog;
 
     constructor() {
         this._plannerContainer = document.getElementById('planner-tool');
-        this._saveDialog = document.getElementById('save-dialog');
         this._createButton = document.getElementById('create-plan') as HTMLButtonElement;
         this._saveButton = document.getElementById('save-plan') as HTMLButtonElement;
         this._state = new ToolState();
+        this._dialog = new CompletePlanDialog();
     }
 
-    init() {
-
-        //rivets.formatters.summary = function (value: string[]) {
-        //    const length = value.length;
-        //    return `You have entered ${length} ${length == 1 ? 'task' : 'tasks'}`;
-        //}
+     init() {
 
         ko.applyBindings(this._state, this._plannerContainer);
 
         let that = this;
         this._createButton.addEventListener('click', function (e: Event) {
             that._state.isNew(false);
-
-            console.log(that._state)
         });
 
-        this._saveButton.addEventListener('click', function (e: Event) {
-            ko.applyBindings(that._state, that._saveDialog);
-            $(that._saveDialog).modal('show');
-            that._state = new ToolState();
-            ko.applyBindings(that._state, that._plannerContainer);
+         this._saveButton.addEventListener('click', async function (e: Event) {
+
+            let tasks = that._state.tasks() as string[];
+            let result = await that._dialog.show(tasks);
+
+            if (result.result === true) {
+                that._state.reset();
+            }
+
+             that._dialog.hide();
+          
         });
     }
 }
@@ -52,13 +52,78 @@ class ToolState {
         return navigator.onLine;
     }
 
-    addTask(e: Event, item: any): void {
-        let state = item.state();
-        state.tasks().push(state.task());
-        state.task('');
-
-        const canSave = state.tasks().length > 0
-        state.canSave(canSave);
+    reset() {
+        this.isNew(true);
+        this.task('');
+        this.tasks.removeAll();
+        this.canSave(false);
     }
-    
+
+    addTask(): void {
+        const t = this.task();
+        this.tasks.push(t);
+        this.task('');
+
+        const canSave = this.tasks().length > 0
+        this.canSave(canSave);
+    }    
+}
+
+
+class DialogState {
+    isOffline = ko.observable(false);
+    tasks = ko.observableArray();
+
+    changeTasks(tasks: string[]) {
+        this.tasks.removeAll();
+        tasks.forEach(t => this.tasks.push(t));
+    }
+}
+
+
+
+class CompletePlanDialog {
+
+    private _dfd: JQueryDeferred<CompletePlanDialogResult>;
+
+    private _dialogSelector = 'save-dialog';
+    private _contentSelector = 'dialog-content';
+    private _saveSelector = 'save-button';
+
+    private _saveButton: HTMLButtonElement;
+    private _dialogElement: HTMLElement;
+    private _dialogContentContainer: HTMLElement;
+    private _dialogState: DialogState = new DialogState();
+
+    constructor() {
+        let that = this;
+
+        this._dialogElement = document.getElementById(this._dialogSelector);
+        this._dialogContentContainer = document.getElementById(this._contentSelector);
+        this._saveButton = document.getElementById(this._saveSelector) as HTMLButtonElement;
+        ko.applyBindings(this._dialogState, this._dialogContentContainer);
+
+        $(this._dialogElement).on('hidden.bs.modal', function (e) {            
+            that._dfd.resolve({ result: false });
+        });
+
+        this._saveButton.addEventListener("click", (e) => {
+            that._dfd.resolve({ result: true });
+        });
+    }
+
+    show(tasks: string[]): JQueryPromise<CompletePlanDialogResult> {
+        this._dfd = $.Deferred<CompletePlanDialogResult>();
+        this._dialogState.changeTasks(tasks);
+        $(this._dialogElement).modal('show');
+        return this._dfd.promise();
+    }
+
+    hide(): void {
+        $(this._dialogElement).modal('hide');
+    }
+}
+
+export interface CompletePlanDialogResult {
+    result: boolean;
 }
